1ca328e57619ca90223a7c98821997852264bdbd
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-10-07 20:08:06 +0000

    adding tweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,16 +1,37 @@
 "use strict";
 
-function countRabbits() {
-  for (let i = 0; i <= 3; i++) {
-    alert("Rabbit number " + i);
-  }
-}
+const tweetJSON = require('../server/data-files/initial-tweets.json');
 
-function handler1() {
-  alert('Thanks!');
-};
+let tweetData = JSON.parse(tweetJSON);
 
-function handler2() {
-  alert('Thanks!!!!!!!!!!!!');
+$( document ).ready(function() {
+
+  $('main').append(createTweetElement());
+
+});
+
+/*
+let div = document.createElement('div');
+div.innerHTML = "<p>Helllo there!!!</p>";
+document.body.append(div);
+*/
+function createTweetElement(data) {
+  let tweet = new DocumentFragment()
+
+  let tweetArticle = document.createElement('article');
+
+
+  let tweetHeader = document.createElement('header');
+  tweetHeader.innerHTML = "<h1>Tweet Example</h1><h2>My Username</h2>";
+
+  let tweetSection = document.createElement('section')
+  tweetSection.innerHTML = `<p>this is the text </p>`
+  
+  tweetArticle.append(tweetHeader);
+  tweetArticle.append(tweetSection);
+
+
+  //document.getElementsByTagName('main')[0].append(tweetArticle);
+  return tweetArticle;
 }
 
