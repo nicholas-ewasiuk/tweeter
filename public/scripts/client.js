e70099908c98a6537139f06ba347f34b3b960bc5
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-10-08 23:40:10 +0000

    fixed css and add responsiveness

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,7 +16,10 @@ function createTweetElement(data) {
   
   tweetArticle.innerHTML = `
   <header>
-    <h1>${data.user.name}</h1>
+    <div class="avatar">
+      <img src="${data.user.avatars}">
+      <h1>${data.user.name}</h1>
+    </div>
     <h2>${data.user.handle}</h2>
   </header>
   <section>
@@ -25,9 +28,9 @@ function createTweetElement(data) {
   <footer>
     <p class="timeago">${timeCreated}</p>
     <div class="icon-drawer">
-     <span class="fas fa-angle-double-down"></span>
-     <span class="fas fa-angle-double-down"></span>
-     <span class="fas fa-angle-double-down"></span>
+     <span class="fas fa-flag fa-xs"></span>
+     <span class="fas fa-retweet fa-sm"></span>
+     <span class="fas fa-heart fa-xs"></span>
     </div>
   </footer>
    `;
@@ -38,5 +41,36 @@ function createTweetElement(data) {
 }
 
 
+$( document ).ready(function() {
+  $( '.tweet-form' ).on('submit', ( function( event ) {
+    
+    event.preventDefault();
 
+    let counter = $( ".counter" )[0].textContent;
 
+    if (counter < 140 && counter >= 0) {
+      const formText = $(this).serialize(); 
+    
+      $.post( "/tweets/", formText )
+        .then(() => {
+          loadTweets();
+        });
+    }
+
+    if (counter == 140) {
+      alert('hello');
+    }
+
+    if (counter < 0) {
+      alert('goodbye');
+    }
+  }));
+
+  loadTweets();
+});
+
+function loadTweets() {
+  $.get( "/tweets/", function( data ) {
+      renderTweets(data);
+  });
+}
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,44 +5,10 @@ $( document ).ready(function() {
     if (event.target.id === "tweet-text") {
       $( ".counter" )[0].textContent = 140 - event.target.value.length;
     }
-  }));
-
-  $( '.tweet-form' ).on('submit', ( function( event ) {
-    event.preventDefault();
-
-    let counter = $( ".counter" )[0].textContent;
-
-    if (counter < 140 && counter > 0) {
-      const formText = $(this).serialize(); 
-    
-      $.post( "/tweets/", formText )
-        .then(() => {
-          loadTweets();
-        });
-    }
-
-    if (counter == 140) {
-      alert('hello');
-    }
-
-    if (counter <= 0) {
-      alert('goodbye');
-    }
-  }));
-
-  loadTweets();
+  }))
 });
 
-function loadTweets() {
-  $.get( "/tweets/", function( data ) {
-      renderTweets(data);
-  });
-}
 
-
-module.exports = {
-  loadTweets
-}
 
 
 
