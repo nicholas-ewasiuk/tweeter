1d3b2765c0e65a111064c3fba71129121e017b10
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-10-09 01:37:31 +0000

    counter color change

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,6 +6,12 @@ $( document ).ready(function() {
     if (event.target.id === "tweet-text") {
       $( ".counter" )[0].textContent = 140 - event.target.value.trim().length;
     }
+    if ($( ".counter" )[0].textContent < 0) {
+      $( ".counter" ).css("color", "red");
+    }
+    if ($( ".counter" )[0].textContent >= 0) {
+      $( ".counter" ).css("color", "#545149");
+    }
   }))
 });
 
